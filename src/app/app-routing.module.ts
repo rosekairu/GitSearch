a3c2807b73ad014a4d0bf0bcdb9f5c05945f297a
Rose Kairu <rosekairu@gmail.com>
2020-05-16 04:54:36 +0300

    imported routes to routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,24 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UsersComponent } from '../app/Components/users/users.component';
+import { RepositoriesComponent } from '../app/Components/repositories/repositories.component';
+import { NotFoundComponent } from '../app/Components/not-found/not-found.component';
+import { AboutComponent } from '../app/Components/about/about.component';
+//import { from } from 'rxjs';
+//import { from } from 'rxjs';
+//import { User } from './user';
 
+const routes: Routes = [
+  { path: 'about', component: AboutComponent },
+  { path: 'user-profile', component: UsersComponent },
+  { path: 'repo-search', component: RepositoriesComponent },
+  { path: '', redirectTo: 'user-profile', pathMatch: 'full' },
 
-const routes: Routes = [];
+  { path: '**', component: NotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
