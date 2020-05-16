e5a4c832319f057b085f3b77f2a866161568718d
Rose Kairu <rosekairu@gmail.com>
2020-05-16 04:56:30 +0300

    modified app module ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { ProfileService } from './Services/profile.service';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { FooterComponent } from './Components/footer/footer.component';
+import { SearchFormComponent } from './Components/search-form/search-form.component';
+import { DatePipe } from './date.pipe';
+import { HighlightDirective } from './highlight.directive';
+//import { from } from 'rxjs';
+import { AboutComponent } from './Components/about/about.component';
+import { UsersComponent } from './Components/users/users.component';
+import { RepositoriesComponent } from './Components/repositories/repositories.component';
+import { NotFoundComponent } from './Components/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    SearchFormComponent,
+    DatePipe,
+    HighlightDirective,
+    AboutComponent,
+    UsersComponent,
+    RepositoriesComponent,
+    NotFoundComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
+  providers: [ProfileService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
